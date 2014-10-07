6ff441898ded942891afeebd79f59a05f36a5825
Johani Vajakas <mindthecap@gmail.com>
2014-10-07 11:54:48 +0300

    build changed

--- POS/src/ee/ut/math/tvt/VTKBTarkvara/Intro.java
+++ POS/src/ee/ut/math/tvt/VTKBTarkvara/Intro.java
@@ -1,7 +1,9 @@
 package ee.ut.math.tvt.VTKBTarkvara;
 
 public class Intro {
-	public static void main(String[] args){
-		System.out.println("Tere");
-	}
+	 public static void main(String[] args){
+		 
+		 
+	 }
+	
 }
