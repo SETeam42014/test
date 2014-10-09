95116c50abc00bf9ff9a1de56ada40c0dabe5f79
Johani Vajakas <mindthecap@gmail.com>
2014-10-09 21:57:36 +0300

    Formatted code

--- POS/src/ee/ut/math/tvt/SETeam42014/Intro.java
+++ POS/src/ee/ut/math/tvt/SETeam42014/Intro.java
@@ -2,22 +2,22 @@ package ee.ut.math.tvt.SETeam42014;
 
 import java.io.IOException;
 
-
 import ee.ut.math.tvt.SETeam42014.IntroUI;
 
 public class Intro {
-	 public static void main(String[] args){
-		 IntroUI userInterface = new IntroUI();
-		 System.out.println("TERE");
-		 //if you want to run this programm, rightclick build.xml -- run as
-		 //-- ant target (the second option). from targets select "run" and not "build".
+	public static void main(String[] args) {
+		IntroUI userInterface = new IntroUI();
+		System.out.println("TERE");
+		// if you want to run this programm, rightclick build.xml -- run as
+		// -- ant target (the second option). from targets select "run" and not
+		// "build".
 
-		 try {
+		try {
 			userInterface.getPropertiesValues();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
-	 }	
+	}
 }
--- POS/src/ee/ut/math/tvt/SETeam42014/IntroUI.java
+++ POS/src/ee/ut/math/tvt/SETeam42014/IntroUI.java
@@ -16,93 +16,92 @@ import javax.swing.JFrame;
 import javax.swing.JTabbedPane;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
+
 public class IntroUI {
-
-
-
 
 	/**
 	 * Graphical user interface of the sales system.
-	 * @return 
+	 * 
+	 * @return
 	 */
 
-	
-	public String getPropertiesValues() throws IOException{
-		 
-		 String result="";
-		 Properties properties = new Properties();
-		 String propertiesFileName = "version.properties";
-		 
-		 InputStream inputStream = getClass().getClassLoader().getResourceAsStream(propertiesFileName);
-		 properties.load(inputStream);
-		 if (inputStream == null){
-			 throw new FileNotFoundException("Property file " + propertiesFileName + " not found in the directory");
-		 }
-		 
-		 Date time = new Date (System.currentTimeMillis());
-		 
-		 //get property value and print it out
-		 String buildNumber = properties.getProperty("build.number");
-		 
-		 result=buildNumber;
-		 
-		 System.out.println(result + "\nProgram ran on " + time);
-		 
-		 return result;
-	 }
-	  public void SalesSystemUI() {
-	 
+	public String getPropertiesValues() throws IOException {
 
-	    setTitle("Sales system");
+		String result = "";
+		Properties properties = new Properties();
+		String propertiesFileName = "version.properties";
 
-	    // set L&F to the nice Windows style
-	    try {
-	      UIManager.setLookAndFeel(new WindowsClassicLookAndFeel());
+		InputStream inputStream = getClass().getClassLoader()
+				.getResourceAsStream(propertiesFileName);
+		properties.load(inputStream);
+		if (inputStream == null) {
+			throw new FileNotFoundException("Property file "
+					+ propertiesFileName + " not found in the directory");
+		}
 
-	    } catch (UnsupportedLookAndFeelException e1) {
-	      //log.warn(e1.getMessage());
-	    }
+		Date time = new Date(System.currentTimeMillis());
 
-	    drawWidgets();
+		// get property value and print it out
+		String buildNumber = properties.getProperty("build.number");
 
-	    // size & location
-	    int width = 600;
-	    int height = 400;
-	    setSize(width, height);
-	    Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
-	    setLocation((screen.width - width) / 2, (screen.height - height) / 2);
+		result = buildNumber;
 
-	    addWindowListener(new WindowAdapter() {
-	      @Override
-	      public void windowClosing(WindowEvent e) {
-	        System.exit(0);
-	      }
-	    });
-	  }
-	  
+		System.out.println(result + "\nProgram ran on " + time);
 
-	  private void addWindowListener(WindowAdapter windowAdapter) {
+		return result;
+	}
+
+	public void SalesSystemUI() {
+
+		setTitle("Sales system");
+
+		// set L&F to the nice Windows style
+		try {
+			UIManager.setLookAndFeel(new WindowsClassicLookAndFeel());
+
+		} catch (UnsupportedLookAndFeelException e1) {
+			// log.warn(e1.getMessage());
+		}
+
+		drawWidgets();
+
+		// size & location
+		int width = 600;
+		int height = 400;
+		setSize(width, height);
+		Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+		setLocation((screen.width - width) / 2, (screen.height - height) / 2);
+
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+	}
+
+	private void addWindowListener(WindowAdapter windowAdapter) {
 		// TODO Auto-generated method stub
-		
-	  ;}
+
+		;
+	}
 
 	private void setLocation(int i, int j) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	private void setSize(int width, int height) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	private void setTitle(String string) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	private void drawWidgets() {
 
-	  }
+	}
 }
-
