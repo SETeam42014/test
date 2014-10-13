b50f25a38bf4dc7a4b7c7dad79fee47cf83f20d1
Johani Vajakas <mindthecap@gmail.com>
2014-10-13 21:33:43 +0300

    Removed not used import.

--- POS/src/ee/ut/math/tvt/SETeam42014/Intro.java
+++ POS/src/ee/ut/math/tvt/SETeam42014/Intro.java
@@ -1,7 +1,5 @@
 package ee.ut.math.tvt.SETeam42014;
 
-import java.io.IOException;
-
 import org.apache.log4j.Logger;
 
 import ee.ut.math.tvt.SETeam42014.IntroUI;
@@ -11,17 +9,16 @@ public class Intro {
 	private static final Logger log = Logger.getLogger(Intro.class);
 
 	public static void main(String[] args) {
-		//just some politeness
+		// just some politeness
 		System.out.println("TERE");
-		//IntroUi used to gei GUI
+		// IntroUi used to gei GUI
 		IntroUI userInterface = new IntroUI();
-		//Gets started classname from log
+		// Gets started classname from log
 		log.info("POS started, class name: " + log.getName());
 
 		// if you want to run this programm, rightclick build.xml -- run as
 		// -- ant target (the second option). from targets select "run" and not
 		// "build".
-
 
 	}
 }
