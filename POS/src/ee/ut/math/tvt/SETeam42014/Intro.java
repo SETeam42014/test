c9dc2a98c7075a6ada024f8ad74f4bd4da338247
Johani Vajakas <mindthecap@gmail.com>
2014-10-13 21:59:39 +0300

    Added timestamp (and formatted code)

--- POS/src/ee/ut/math/tvt/SETeam42014/Intro.java
+++ POS/src/ee/ut/math/tvt/SETeam42014/Intro.java
@@ -1,5 +1,7 @@
 package ee.ut.math.tvt.SETeam42014;
 
+import java.util.Date;
+
 import org.apache.log4j.Logger;
 
 import ee.ut.math.tvt.SETeam42014.IntroUI;
@@ -9,16 +11,17 @@ public class Intro {
 	private static final Logger log = Logger.getLogger(Intro.class);
 
 	public static void main(String[] args) {
-		// just some politeness
-		System.out.println("TERE");
-		// IntroUi used to gei GUI
+		// Datetime used for timestamp
+		Date date = new Date();
+		// Program running log entry
+		log.info("POS started, class name: " + log.getName() + ". Timestamp: "
+				+ date.toString());
+		// testing purposes
+		// System.out.println("TERE");
+
+		// IntroUi used to get GUI
 		IntroUI userInterface = new IntroUI();
 		// Gets started classname from log
-		log.info("POS started, class name: " + log.getName());
-
-		// if you want to run this programm, rightclick build.xml -- run as
-		// -- ant target (the second option). from targets select "run" and not
-		// "build".
 
 	}
 }
