7800f6be29fcd03f357772eb7bdf01112fa8e505
Johani Vajakas <mindthecap@gmail.com>
2014-10-22 14:48:59 +0300

    Implemented HistoryItem

---
+++ POS/src/ee/ut/math/tvt/salessystem/domain/data/HistoryItem.java
@@ -0,0 +1,80 @@
+/**
+ * 
+ */
+package ee.ut.math.tvt.salessystem.domain.data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @author Johani
+ *
+ */
+public class HistoryItem implements DisplayableItem, Serializable {
+	private static long nextId;
+	private long id;
+	private Date endDate;
+	private Date startDate;
+	private double sum;
+	private int state;
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public double getSum() {
+		return sum;
+	}
+
+	public void setSum(double sum) {
+		this.sum = sum;
+	}
+
+	public int getState() {
+		return state;
+	}
+
+	public void setState(int state) {
+		this.state = state;
+	}
+
+	public HistoryItem() {
+		this.id = nextId++;
+		this.startDate = new Date();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see ee.ut.math.tvt.salessystem.domain.data.DisplayableItem#getId()
+	 */
+	@Override
+	public Long getId() {
+		// TODO Auto-generated method stub
+		return this.id;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see ee.ut.math.tvt.salessystem.domain.data.DisplayableItem#getName()
+	 */
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
