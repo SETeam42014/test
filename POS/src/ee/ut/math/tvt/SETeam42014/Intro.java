4ab7b853dbad4678ba8880701b1b7e0f730e00a6
Johani Vajakas <mindthecap@gmail.com>
2014-10-27 22:38:32 +0200

    ???

--- POS/src/ee/ut/math/tvt/SETeam42014/Intro.java
+++ POS/src/ee/ut/math/tvt/SETeam42014/Intro.java
@@ -5,21 +5,41 @@ import java.util.Date;
 import org.apache.log4j.Logger;
 
 import ee.ut.math.tvt.SETeam42014.IntroUI;
+import ee.ut.math.tvt.salessystem.domain.controller.SalesDomainController;
+import ee.ut.math.tvt.salessystem.domain.controller.impl.SalesDomainControllerImpl;
+import ee.ut.math.tvt.salessystem.ui.ConsoleUI;
+import ee.ut.math.tvt.salessystem.ui.SalesSystemUI;
 
 public class Intro {
 
 	private static final Logger log = Logger.getLogger(Intro.class);
+	private static final String MODE = "console";
 
 	public static void main(String[] args) {
-		// Datetime used for timestamp
-		Date date = new Date();
-		// Program running log entry
-		log.info("POS started, class name: " + log.getName() + ". Timestamp: "
-				+ date.toString());
-		// testing purposes
-		// System.out.println("TERE");
 
-		// IntroUi used to get GUI
-		IntroUI userInterface = new IntroUI();
+		final SalesDomainController domainController = new SalesDomainControllerImpl();
+
+		if (args.length == 1 && args[0].equals(MODE)) {
+			log.debug("Mode: " + MODE);
+
+			ConsoleUI cui = new ConsoleUI(domainController);
+			cui.run();
+		} else {
+
+			// IntroUI introUI = new IntroUI();
+			// introUI.setVisible(true);
+			// introUI.setAlwaysOnTop(true);
+
+			final SalesSystemUI ui = new SalesSystemUI(domainController);
+			ui.setVisible(true);
+
+			// introUI.setAlwaysOnTop(false);
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			// introUI.setVisible(false);
+		}
 	}
 }
