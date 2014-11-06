7b5928e91f4a70e32b9818e7a178aadf7be15677
mindthecap <mindthecap@gmail.com>
2014-11-06 10:13:28 +0200

    Removed SalesSystemModel variable.

--- POS/src/ee/ut/math/tvt/salessystem/ui/SalesSystemUI.java
+++ POS/src/ee/ut/math/tvt/salessystem/ui/SalesSystemUI.java
@@ -31,8 +31,6 @@ public class SalesSystemUI extends JFrame {
 
 	private final SalesDomainControllerImpl domainController;
 
-	private final SalesSystemModel model;
-
 	// Instances of tab classes
 	private PurchaseTab purchaseTab;
 	private HistoryTab historyTab;
@@ -47,7 +45,6 @@ public class SalesSystemUI extends JFrame {
 	 */
 	public SalesSystemUI(SalesDomainControllerImpl domainController) {
 		this.domainController = domainController;
-		this.model = domainController.getModel();
 
 		// Create single instances of the tab classes
 		historyTab = new HistoryTab(domainController);
