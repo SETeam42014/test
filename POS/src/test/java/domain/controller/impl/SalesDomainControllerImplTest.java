a1cbe19d9962dedc608fa365adffff0fc960fb2c
Johani Vajakas <mindthecap@gmail.com>
2014-11-18 10:30:48 +0200

    Meh.

--- POS/src/test/java/domain/controller/impl/SalesDomainControllerImplTest.java
+++ POS/src/test/java/domain/controller/impl/SalesDomainControllerImplTest.java
@@ -45,8 +45,115 @@ public class SalesDomainControllerImplTest {
 	public void tearDown() throws Exception {
 	}
 
+	/**
+	 * Test method for {@link ee.ut.math.tvt.salessystem.domain.controller.impl.SalesDomainControllerImpl#startNewPurchase()}.
+	 */
 	@Test
-	public void test() {
+	public void testStartNewPurchase() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link ee.ut.math.tvt.salessystem.domain.controller.impl.SalesDomainControllerImpl#cancelCurrentPurchase()}.
+	 */
+	@Test
+	public void testCancelCurrentPurchase() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link ee.ut.math.tvt.salessystem.domain.controller.impl.SalesDomainControllerImpl#submitCurrentPurchase(java.util.List)}.
+	 */
+	@Test
+	public void testSubmitCurrentPurchaseListOfSoldItem() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link ee.ut.math.tvt.salessystem.domain.controller.impl.SalesDomainControllerImpl#loadWarehouseState()}.
+	 */
+	@Test
+	public void testLoadWarehouseState() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link ee.ut.math.tvt.salessystem.domain.controller.impl.SalesDomainControllerImpl#endSession()}.
+	 */
+	@Test
+	public void testEndSession() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link ee.ut.math.tvt.salessystem.domain.controller.impl.SalesDomainControllerImpl#SalesDomainControllerImpl()}.
+	 */
+	@Test
+	public void testSalesDomainControllerImpl() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link ee.ut.math.tvt.salessystem.domain.controller.impl.SalesDomainControllerImpl#getModel()}.
+	 */
+	@Test
+	public void testGetModel() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link ee.ut.math.tvt.salessystem.domain.controller.impl.SalesDomainControllerImpl#submitCurrentPurchase()}.
+	 */
+	@Test
+	public void testSubmitCurrentPurchase() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link ee.ut.math.tvt.salessystem.domain.controller.impl.SalesDomainControllerImpl#getStockTableModel()}.
+	 */
+	@Test
+	public void testGetStockTableModel() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link ee.ut.math.tvt.salessystem.domain.controller.impl.SalesDomainControllerImpl#updateStockTableModel()}.
+	 */
+	@Test
+	public void testUpdateStockTableModel() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link ee.ut.math.tvt.salessystem.domain.controller.impl.SalesDomainControllerImpl#updateHistoryTableModel()}.
+	 */
+	@Test
+	public void testUpdateHistoryTableModel() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link ee.ut.math.tvt.salessystem.domain.controller.impl.SalesDomainControllerImpl#getCurrentPurchaseInfoTableModel()}.
+	 */
+	@Test
+	public void testGetCurrentPurchaseInfoTableModel() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link ee.ut.math.tvt.salessystem.domain.controller.impl.SalesDomainControllerImpl#addStockItem(ee.ut.math.tvt.salessystem.domain.data.StockItem)}.
+	 */
+	@Test
+	public void testAddStockItem() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link ee.ut.math.tvt.salessystem.domain.controller.impl.SalesDomainControllerImpl#getHistoryItemById(long)}.
+	 */
+	@Test
+	public void testGetHistoryItemById() {
 		fail("Not yet implemented"); // TODO
 	}
 
