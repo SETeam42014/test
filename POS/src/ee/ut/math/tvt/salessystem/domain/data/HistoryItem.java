34ef463e65b370e7fb27681f7a7e4de84a094970
Johani Vajakas <mindthecap@gmail.com>
2014-11-22 11:46:32 +0200

    Removed nullpointerexception check from constructor as it is not needed.

--- POS/src/ee/ut/math/tvt/salessystem/domain/data/HistoryItem.java
+++ POS/src/ee/ut/math/tvt/salessystem/domain/data/HistoryItem.java
@@ -37,9 +37,6 @@ public class HistoryItem implements Cloneable, DisplayableItem {
 	private List<SoldItem> items;
 
 	public HistoryItem(List<SoldItem> purchase) {
-		if (purchase == null) {
-			throw new NullPointerException();
-		}
 		this.date = new Date();
 		this.items = purchase;
 		for (SoldItem soldItem : purchase) {
