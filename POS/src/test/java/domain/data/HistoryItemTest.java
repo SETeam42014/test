a807e8f7d9afe07c64611fea6f46c41de39f1a71
Johani Vajakas <mindthecap@gmail.com>
2014-11-23 16:23:43 +0200

    Implemented some tests / fixed some.

--- POS/src/test/java/domain/data/HistoryItemTest.java
+++ POS/src/test/java/domain/data/HistoryItemTest.java
@@ -18,10 +18,6 @@ import ee.ut.math.tvt.salessystem.domain.data.HistoryItem;
 import ee.ut.math.tvt.salessystem.domain.data.SoldItem;
 import ee.ut.math.tvt.salessystem.domain.data.StockItem;
 
-/**
- * @author Johani
- *
- */
 public class HistoryItemTest {
 
 	private StockItem stockItem1;
@@ -33,14 +29,12 @@ public class HistoryItemTest {
 	private SoldItem soldItem5;
 	private double sum;
 	private List<SoldItem> soldItems = new ArrayList<SoldItem>();
+	HistoryItem historyItem;
 
-	/**
-	 * @throws java.lang.Exception
-	 */
 	@Before
 	public void setUp() throws Exception {
 		stockItem1 = new StockItem((long) 1, "Lauaviin", "Alkohol", 15.0, 3);
-		stockItem2 = new StockItem((long) 1, "Hapukurk", "Lisand", 2.0, 12);
+		stockItem2 = new StockItem((long) 2, "Hapukurk", "Lisand", 2.0, 12);
 		soldItem1 = new SoldItem(stockItem1, 1);
 		soldItem2 = new SoldItem(stockItem2, 1);
 		soldItem3 = new SoldItem(stockItem1, 2);
@@ -50,55 +44,39 @@ public class HistoryItemTest {
 		soldItems.add(soldItem3);
 		soldItems.add(soldItem4);
 		sum = 1 * 2.0 + 2 * 15.0 + 3 * 2;
-		// soldItems.add(soldItem1);
 	}
 
-	/**
-	 * @throws java.lang.Exception
-	 */
 	@After
 	public void tearDown() throws Exception {
 	}
 
-	/**
-	 * Test method for
-	 * {@link ee.ut.math.tvt.salessystem.domain.data.HistoryItem#HistoryItem(java.util.List)}
-	 * .
-	 */
 	@Test
 	public void testHistoryItemConstructorWithSoldItems() {
-		HistoryItem historyItem = new HistoryItem(soldItems);
+		historyItem = new HistoryItem(soldItems);
 		assertEquals(soldItems, historyItem.getItems());
 	}
 
 	@Test(expected = NullPointerException.class)
 	public void testHistoryItemConstructorWithNullItems() {
-		HistoryItem historyItem = new HistoryItem(null);
+		historyItem = new HistoryItem(null);
 	}
 
 	@Test
 	public void testHistoryItemConstructorWithNoVariables() {
-		HistoryItem historyItem = new HistoryItem();
+		historyItem = new HistoryItem();
 		List<SoldItem> items = historyItem.getItems();
 		assertEquals(0, items.size());
 	}
 
 	@Test
-	public void testGetDate() {
-		Date date = new Date();
-		HistoryItem historyItem = new HistoryItem(new ArrayList<SoldItem>());
-		assertEquals(date, historyItem.getDate());
-	}
-
-	@Test
 	public void testGetSum() {
-		HistoryItem historyItem = new HistoryItem(soldItems);
+		historyItem = new HistoryItem(soldItems);
 		assertTrue(this.sum == historyItem.getSum());
 	}
 
 	@Test
 	public void testGetSumWithNoItems() {
-		HistoryItem historyItem = new HistoryItem(new ArrayList<SoldItem>());
+		historyItem = new HistoryItem(new ArrayList<SoldItem>());
 		assertEquals(0.0, historyItem.getSum(), 0.00001);
 	}
 
--- POS/src/test/java/domain/model/StockTableModelTest.java
+++ POS/src/test/java/domain/model/StockTableModelTest.java
@@ -3,24 +3,41 @@
  */
 package test.java.domain.model;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.NoSuchElementException;
 
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
+import ee.ut.math.tvt.salessystem.domain.data.SoldItem;
+import ee.ut.math.tvt.salessystem.domain.data.StockItem;
+import ee.ut.math.tvt.salessystem.domain.exception.OutOfStockException;
 import ee.ut.math.tvt.salessystem.domain.model.StockTableModel;
 
-/**
- * @author Johani
- *
- */
 public class StockTableModelTest {
+	private StockItem stockItem1;
+	private StockItem stockItem2;
+	private StockItem stockItem3;
+	private StockItem stockItem4;
+	private SoldItem soldItem1;
+	private SoldItem soldItem2;
+	private SoldItem soldItem3;
+	private StockTableModel stock;
 
 	@Before
 	public void setUp() {
+		stockItem1 = new StockItem((long) 1, "Lauaviin", "Alkohol", 15.0, 3);
+		stockItem2 = new StockItem((long) 2, "Hapukurk", "Lisand", 2.0, 12);
+		stockItem3 = new StockItem((long) 3, "Lauaviin", "Jook", 6.2, 42);
+		stockItem4 = new StockItem((long) 1, "Koorejuust", "Toit", 10.0, 12);
+		soldItem1 = new SoldItem(stockItem1, 1);
+		soldItem2 = new SoldItem(stockItem2, 1);
+		soldItem3 = new SoldItem(stockItem1, 2);
+		stock = new StockTableModel();
 	}
 
 	@After
@@ -29,8 +46,64 @@ public class StockTableModelTest {
 
 	@Test
 	public void testStockTableModelDefaultConstructor() {
-		StockTableModel stock = new StockTableModel();
 		assertTrue(stock.getRowCount() == 0);
+	}
+
+	@Test
+	public void testStockTableAddItem() {
+		stock.addItem(stockItem1);
+		assertTrue(stock.getRowCount() == 1);
+		assertEquals(stockItem1, stock.getItemAt(0));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testStockTableAddItemWithSameId() {
+		stock.addItem(stockItem1);
+		stock.addItem(stockItem4);
+	}
+
+	@Test
+	public void testStockTableAddItemWithSameName() {
+		stock.addItem(stockItem1);
+		stock.addItem(stockItem3);
+		// TODO
+		fail("Unhandled input combination");
+	}
+
+	@Test(expected = NoSuchElementException.class)
+	public void testStockTableSellItemWithNoStock() throws OutOfStockException {
+		stock.sellItem(soldItem1);
+	}
+
+	@Test(expected = NoSuchElementException.class)
+	public void testStockTableSellItemWithNotInStock()
+			throws OutOfStockException {
+		stock.addItem(stockItem2);
+		stock.sellItem(soldItem1);
+	}
+
+	@Test(expected = OutOfStockException.class)
+	public void testStockTableSellItemWithNotEnoughInStock()
+			throws OutOfStockException {
+		stock.addItem(stockItem1);
+		stock.sellItem(new SoldItem(stockItem1, 4));
+	}
+
+	@Test
+	public void testStockTableSellItemWithEnoughInStock()
+			throws OutOfStockException {
+		int goodsInStock = stockItem2.getQuantity();
+		int goodsLeftInStock;
+		stock.addItem(stockItem2);
+		stock.sellItem(new SoldItem(stockItem2, 4));
+		goodsLeftInStock = stock.getItemAt(0).getQuantity();
+		assertEquals(4, goodsInStock - goodsLeftInStock);
+	}
+
+	@Test
+	public void testStockTableModelToString() {
+		String toString = stock.toString();
+		assertTrue(toString.length() > 0);
 	}
 
 }
